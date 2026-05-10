33e91ec9141bd9f20b48a435ac6dc949bf5a1bd9
CSerVN <summerthinh3@gmail.com>
2026-05-10 15:57:00 +0700

    add some content

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Phuoc Thinh Portfolio
       </h1>
       <p className="mb-4">
-        {`Welcome to my portfolio. Here, I share my blog and notebook, documenting my educational journey. Let's sit back, relax and enjoy the moment together!`}
+        {`Welcome to my portfolio. Here, I share my blog and notebook, documenting my educational journey. I'm a firm believer in interdisciplinary wisdom, so you'll also find me sharing fascinating breakthroughs in other fields here\nLet's sit back, relax and enjoy the moment together!`}
       </p>
       <div className="my-8">
         <BlogPosts />
