af324f9e2c66793ddb07d42b0e7cf0e67811c0db
CSerVN <summerthinh3@gmail.com>
2026-05-10 11:19:13 +0700

    change the menu tab

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,5 @@
 import { BlogPosts } from 'app/components/posts'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'Blog',
@@ -9,6 +10,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <nav className="mb-4">
+        <Link href="https://vercel.com" className="text-blue-500 hover:underline">
+          Vercel
+        </Link>
+      </nav>
       <BlogPosts />
     </section>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Welcome to my portfolio. In there, I'll share my Welcome to my portfolio. Here, I share my blog and notebook, documenting my educational journey. Let's sit back, relax and enjoy the moment together!`}
       </p>
       <div className="my-8">
         <BlogPosts />
